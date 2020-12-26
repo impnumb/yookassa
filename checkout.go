9167fe3807b660980e774e1c53f7af8fd72e5837
Alex <impnumb@gmail.com>
2020-12-27 01:35:18 +0800

    add checkout

--- checkout.go
+++ checkout.go
@@ -2,3 +2,15 @@ package yookassa
 
 //Endpoint is API Endpoint
 const Endpoint = "https://api.yookassa.ru/v3/"
+
+//Checkout is checkout struct
+type Checkout struct {
+	ShopID        int
+	SecurityToken string
+	OAuthToken    string
+}
+
+//NewCheckout create Checkout
+func NewCheckout() {
+
+}
